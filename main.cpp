1d483b0ac102a82ee750b778e453153d03a183a9
Grmcrkrs <grmcrkrs@gmail.com>
2021-03-20 17:07:11 -0500

    added farToCelcius conversion function added conversion function before writing to vector

--- main.cpp
+++ main.cpp
@@ -6,14 +6,23 @@
 
 using namespace std;
 
+//function that converts Fahrenheit to Celcius
+//takes one int argument, the temperature in Fahrenheit
+//returns an int (the converted temperature in Celcius)
+int farToCel(int cityTempFahrenheit){
+    double tempCelcius;
+    tempCelcius = (cityTempFahrenheit - 32) * (5.0 / 9.0);
+    return tempCelcius;
+
+}
+
 int main() {
-    pair <string, int> cityAndSize; //stuct holding a pair, like a tuple
+    pair <string, int> cityAndTemp; //stuct holding a pair, like a tuple
     vector<pair<string, int>> holderVectorPair; //vector of pairs
 
     string myText;
     string cityName;
-    int size;
-    int iterator = 0;
+    int temperature;
 
     //opens file, assumes file is in same directory
     ifstream ReadFile("sampledata.txt");
@@ -29,9 +38,11 @@ int main() {
             //into an input stream name ss (StringStream)
             istringstream ss(myText);
 
-            ss >> cityName >> size;
-            cityAndSize = make_pair(cityName, size);
-            holderVectorPair.push_back(cityAndSize);
+            ss >> cityName >> temperature;
+            //convert before writing to pair/vector
+            temperature = farToCel(temperature);
+            cityAndTemp = make_pair(cityName, temperature);
+            holderVectorPair.push_back(cityAndTemp);
 
         }
 
